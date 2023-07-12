2d3e364022ff0c218334ac5a24b4ce862d65275c
zhangxiaofei <xtzyf2011@163.com>
2023-07-12 18:18:38 +0800

    add git instructions

--- instructions.js
+++ instructions.js
@@ -4,10 +4,13 @@
     //git rebase
     //git stage
     //git pull
-    //git push 
-    //git branch:查看分支
-    //git status:查看修改状态
-    //git add
+    //git push
+    //git branch -a/-v
+    //git status
     //git stash list/show/save/pop/apply
-    
+    //git add .
+    //git fetch
+    //git commit -m ""
+    //github use personal access token not username and pwd from 2021
+    //add token to .git/config
 </script>
